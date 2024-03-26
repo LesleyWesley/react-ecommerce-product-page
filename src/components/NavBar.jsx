710ea759c11ba8541f61507abef9a428ea804cc1
lesley_w <lrwesley92@gmail.com>
2024-03-25 22:34:20 -0500

    Added menu links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,18 @@ function NavBar () {
       //mobile toggle
 
       //actual navigation links
-      
+      <div className="mobile-menu-bg">
+          <div className="menu-container">
+              <ul>
+                  <li><a href="#">Collections</a></li>
+                  <li><a href="#">Men</a></li>
+                  <li><a href="#">Women</a></li>
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </div>
+      </div>
+
     </nav>
   )
 }
