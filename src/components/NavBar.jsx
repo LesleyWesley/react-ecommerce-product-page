ec8c2d0f1750e054cde773cd6b3f1dc0bacff93d
lesley_w <lrwesley92@gmail.com>
2024-03-28 14:23:56 -0500

    Added state to toggle mobile menu open and closed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,23 @@
 import React from "react"
 
 function NavBar () {
+
+  const [menuOpen, setMenuOpen] = React.useState(false)
+
+  function toggleMenu() {
+    setMenuOpen(prevMenu => !prevMenu)
+  }
+
   return (
     <nav role="navigation">
       {/*mobile toggle*/}
 
-      <button className="menu-button"><img src="icon-menu.svg" alt="menu button" /></button>
+      <button className="menu-button" onClick={toggleMenu}><img src="icon-menu.svg" alt="menu button" /></button>
 
       {/*actual navigation links*/}
       <div className="mobile-menu-bg">
           <div className="menu-container">
-              <ul className="nav-list">
+              <ul className={menuOpen ? "" : "hide"}>
                   <li><a href="#">Collections</a></li>
                   <li><a href="#">Men</a></li>
                   <li><a href="#">Women</a></li>
